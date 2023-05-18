9a3b59804de7ed8260028fd66c3b47cc56aa1157
Thales <dan.console.log@gmail.com>
2023-05-18 11:39:10 +0200

    add duplicates problem

--- linked_lists.js
+++ linked_lists.js
@@ -69,3 +69,22 @@ let findNode = (head, k) => {
 
     return slow;
 }
+
+// Given the head of a sorted linked list, delete all duplicates such that each element appears only once. Return the linked list sorted as well.
+
+// Example 1:
+
+// Input: head = [1,1,2]
+// Output: [1,2]
+
+const deleteDuplicates = (head) => {
+    let dummy = head
+
+    while (dummy && dummy.next) {
+        if (dummy.val === dummy.next.val) dummy.next = dummy.next.next
+        else {
+            dummy = dummy.next
+        }
+    }
+    return head
+}
